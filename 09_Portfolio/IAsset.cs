5f018869bcfb6514c35d73fd35444db8817f75c4
Ortega <Ortega@DESKTOP-2PFGEBL>
2016-09-26 11:03:37 +0200

    Add project files.

---
+++ 09_Portfolio/IAsset.cs
@@ -0,0 +1,6 @@
+﻿namespace _09_Portfolio
+{
+    internal interface IAsset
+    {
+    }
+}
---
+++ 09_Portfolio/Stock.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _09_Portfolio
+{
+    internal class Stock : IAsset
+    {
+        private int numShares;
+        private double pricePerShare;
+        private string symbol;
+        public Stock()
+        {
+        }
+        public Stock( string a, double b, double c)
+        {
+            
+        }
+
+        public int NumShares { get { return numShares; } internal set { numShares = value; } }
+        public double PricePerShare { get { return pricePerShare; } internal set { pricePerShare = value; } }
+        public string Symbol { get { return symbol; } internal set { symbol = value; } }
+
+        internal int GetValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static double TotalValue(Stock[] stocks)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
